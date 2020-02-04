3ffc680782e591a2236b56dfa5fdb28e302950ee
macoredroid <coredroid0401@gmail.com>
2020-02-04 15:58:19 +0800

    update

---
+++ 174/optdp.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public int calculateMinimumHP(int[][] dungeon) {
+        int row=dungeon.length;
+        int col=dungeon[0].length;
+        int[] dp=new int[col+1];
+        for(int i=0;i<dp.length;i++)
+        {
+            dp[i]=Integer.MAX_VALUE;
+        }
+        dp[col-1]=dungeon[row-1][col-1]>0?1:1-dungeon[row-1][col-1];
+        for(int i=row-1;i>=0;i--)
+        {
+            for(int j=col-1;j>=0;j--)
+            {
+                if(i==row-1&&j==col-1)
+                {
+                    continue;
+                }
+                dp[j]=Math.min(dp[j],dp[j+1])-dungeon[i][j];
+                if(dp[j]<=0)
+                {
+                    dp[j]=1;
+                }
+            }
+        }
+        return dp[0];
+
+    }
+}
