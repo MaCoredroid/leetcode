f8e861290a5c01d7859f5d82b33a9801547a2a4a
macoredroid <coredroid0401@gmail.com>
2020-02-08 20:59:06 +0800

    update

---
+++ 102/opt.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    List<List<Integer>> output=new ArrayList<List<Integer>>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        preOrder(root,0);
+        return output;
+    }
+    public void preOrder(TreeNode root,int level)
+    {
+        if(root==null)
+        {
+            return;
+        }
+        if(output.size()==level)
+        {
+            output.add(new ArrayList<Integer>());           
+        }
+        output.get(level).add(root.val);
+        preOrder(root.left,level+1);
+        preOrder(root.right,level+1);
+    }
+}
