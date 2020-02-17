abc8857f49e15c7cd62dce15e2459a9d078b8875
macoredroid <coredroid0401@gmail.com>
2020-02-17 14:35:38 +0800

    update

---
+++ 117/opt1.java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    
+    public Node connect(Node root) {
+        Node dummyHead=new Node(-1), curr=dummyHead, ans=root;
+        while(root!=null)
+        {
+            if(root.left!=null)
+            {
+                curr.next=root.left;
+                curr=curr.next;
+            }
+            if(root.right!=null)
+            {
+                curr.next=root.right;
+                curr=curr.next;
+            }
+            root=root.next;
+            if(root==null)
+            {
+                root=dummyHead.next;
+                dummyHead.next=null;
+                curr=dummyHead;
+            }
+        }
+        return ans;
+    }
+}
