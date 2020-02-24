e14ee6419431e14edd0584fe0a5fa913ef6b7172
macoredroid <coredroid0401@gmail.com>
2020-02-24 21:55:18 +0800

    update

---
+++ 128/opt.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if(nums.length==0)
+        {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int ret=1;
+        int current=1;
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]==nums[i-1])
+            {
+                continue;
+            }
+            if(nums[i]==nums[i-1]+1)
+            {
+                current++;
+            }
+            else
+            {
+                ret=ret>current?ret:current;
+                current=1;
+            }
+        }
+        ret=ret>current?ret:current;
+        return ret;
+    }
+}
