767ff5ac17ecb3baaa26199785377bd3d4b73d47
macoredroid <coredroid0401@gmail.com>
2020-02-27 16:33:45 +0800

    update

---
+++ 145/opt1.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+  public List<Integer> postorderTraversal(TreeNode root) {
+    LinkedList<TreeNode> stack = new LinkedList<>();
+    LinkedList<Integer> output = new LinkedList<>();
+    if (root == null) {
+      return output;
+    }
+
+    stack.add(root);
+    while (!stack.isEmpty()) {
+      TreeNode node = stack.pollLast();
+      output.addFirst(node.val);
+      if (node.left != null) {
+        stack.add(node.left);
+      }
+      if (node.right != null) {
+        stack.add(node.right);
+      }
+    }
+    return output;
+  }
+}
