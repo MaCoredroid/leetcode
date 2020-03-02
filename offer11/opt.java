0a5f94a5adbf6ff70c76674c394fc4ba125a4d2a
macoredroid <coredroid0401@gmail.com>
2020-03-02 21:03:28 +0800

    :update

---
+++ offer11/opt.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minArray(int[] numbers) {
+        int i=0;
+        int j=numbers.length-1;
+        while(i<=j)
+        {
+            int mid=(i+j)/2;
+            if(numbers[mid]<numbers[j])
+            {
+                j=mid;
+            }
+            else if(numbers[mid]>numbers[j])
+            {
+                i=mid+1;
+            }
+            else
+            {
+                j--;
+            }
+        }
+        return numbers[i];
+    }
+}
