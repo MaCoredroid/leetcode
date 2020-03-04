bd1c7a8a85ed2affc35c47361247b44cfa9d798b
macoredroid <coredroid0401@gmail.com>
2020-03-04 20:53:16 +0800

    :update

---
+++ offer20/opt.java
@@ -0,0 +1,44 @@
+class Solution {
+    int i=0;
+    public boolean isNumber(String s) {
+        if(s==null||s.length()==0)
+        {
+            return false;
+        }
+        s=s.trim();
+        boolean A = scanInteger(s), B = false, C = false;
+        if(i<s.length()&&s.charAt(i)=='.')
+        {
+            i++;
+            B=scanUnsignedInteger(s);
+        }
+        if(i<s.length()&&(s.charAt(i)=='E'||s.charAt(i)=='e'))
+        {
+            i++;
+            C=scanInteger(s);
+            if(!C)
+            {
+                return false;
+            }
+        }
+        return i==s.length()&&(A||B);
+
+    }
+    private boolean scanInteger(String s)
+    {
+        if(i<s.length()&&(s.charAt(i)=='+'||s.charAt(i)=='-'))
+        {
+            i++;
+        }
+        return scanUnsignedInteger(s);
+    }
+    private boolean scanUnsignedInteger(String s)
+    {
+        int start=i;
+        while(i<s.length()&&s.charAt(i)>='0'&&s.charAt(i)<='9')
+        {
+            i++;
+        }
+        return i>start;
+    }
+}
