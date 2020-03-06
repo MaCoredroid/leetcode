89fc4954804f4740ad0bc011f3069b99a4f91b6f
macoredroid <coredroid0401@gmail.com>
2020-03-06 10:20:54 +0800

    :CC2-14:add subtab

---
+++ offer24/opt.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null)
+        {
+            return head;
+        }
+        ListNode pre=null;
+        ListNode curr=head;
+        ListNode next=head;
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=pre;
+            pre=curr;
+            curr=next;
+        }
+        return pre;
+
+    }
+}
