8fb23f6c6641d3558ca709a9085a1fc9440f8cf5
macoredroid <coredroid0401@gmail.com>
2020-04-13 18:26:09 +0800

    update

---
+++ 20/opt.java
@@ -0,0 +1,55 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='[')
+            {
+                stack.push(s.charAt(i));
+            }
+            else
+            {
+                if(s.charAt(i)==')')
+                {
+                    if(!stack.empty()&&stack.peek()=='(')
+                    {
+                        stack.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else if(s.charAt(i)=='}')
+                {
+                    if(!stack.empty()&&stack.peek()=='{')
+                    {
+                        stack.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else if(s.charAt(i)==']')
+                {
+                    if(!stack.empty()&&stack.peek()=='[')
+                    {
+                        stack.pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        if(stack.size()>0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
