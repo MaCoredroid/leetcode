625a83ab47deeb4718af614f8d4fd1cc3d1e07c7
macoredroid <coredroid0401@gmail.com>
2021-03-27 22:54:36 +0800

    update

--- 9/20210327.java
+++ 9/20210327.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0){
+        if(x<0||(x%10==0&&x!=0)){
             return false;
         }
         int base=0;
-        int curr=x;
-        while(curr>0){
-            base*=10;
-            base+=curr%10;
-            curr/=10;
+        while(x>base){
+            base=base*10+x%10;
+            x/=10;
         }
-        return base==x;
+        return base==x||x==base/10;
     }
 }
