6fa30e5f0443bc49b1d88399a14c81514f867e22
macoredroid <coredroid0401@gmail.com>
2021-03-29 22:24:46 +0800

    update

---
+++ offer09/20210329.java
@@ -0,0 +1,32 @@
+class CQueue {
+    Stack<Integer> stack1=new Stack<>();
+    Stack<Integer> stack2=new Stack<>();
+    public CQueue() {
+
+    }
+    
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+    
+    public int deleteHead() {
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        if(stack2.isEmpty()){
+            return -1;
+        }else{
+            return stack2.pop();
+        }
+
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
