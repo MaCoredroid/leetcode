270af56a416030980041ebaf8bbac2425e60657d
macoredroid <coredroid0401@gmail.com>
2021-04-30 08:55:39 +0800

    update

---
+++ 445/20210430.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy=new ListNode(-1);
+        ListNode pointer=dummy;
+        int overflow=0;
+        Stack<Integer> stack1=new Stack<>();
+        Stack<Integer> stack2=new Stack<>();
+        while(l1!=null){
+            stack1.push(l1.val);
+            l1=l1.next;
+        }
+        while(l2!=null){
+            stack2.push(l2.val);
+            l2=l2.next;
+        }
+        Stack<Integer> stack3=new Stack<>();
+        while(!stack1.isEmpty()||!stack2.isEmpty()||overflow!=0){
+            int num1=stack1.isEmpty()?0:stack1.pop();
+            int num2=stack2.isEmpty()?0:stack2.pop();
+            int ans=num1+num2+overflow;
+            if(ans>=10){
+                ans-=10;
+                overflow=1;
+            }else{
+                overflow=0;
+            }
+            stack3.push(ans);
+        }
+        while(!stack3.isEmpty()){
+            pointer.next=new ListNode(stack3.pop());
+            pointer=pointer.next;
+        }
+        return dummy.next;
+    }
+}
