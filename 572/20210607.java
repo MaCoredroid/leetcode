0855b4c2ddae452c0d6fe66847ca8e5bb650bdc7
macoredroid <coredroid0401@gmail.com>
2021-06-07 22:41:46 +0800

    update

---
+++ 572/20210607.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null){
+            return false;
+        }
+        return check(root,subRoot)||isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
+    }
+
+    private boolean check(TreeNode root1,TreeNode root2){
+        if(root1==null&&root2==null){
+            return true;
+        }
+        if(root1==null||root2==null){
+            return false;
+        }
+        if(root1.val!=root2.val){
+            return false;
+        }
+        return check(root1.left,root2.left)&&check(root1.right,root2.right);
+    }
+}
