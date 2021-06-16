5c7d0805a985a48e66f694f85769e6c5bd29bdb6
macoredroid <coredroid0401@gmail.com>
2021-06-16 21:01:13 +0800

    update

---
+++ 768/20210616.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        Map<Integer, Integer> count = new HashMap();
+        int ans = 0, nonzero = 0;
+
+        int[] expect = arr.clone();
+        Arrays.sort(expect);
+        for(int i=0;i<arr.length;i++){
+            int x=arr[i];
+            int y=expect[i];
+            count.put(x,count.getOrDefault(x,0)+1);
+            if(count.get(x)==0){
+                nonzero--;
+            }else if(count.get(x)==1){
+                nonzero++;
+            }
+            count.put(y,count.getOrDefault(y,0)-1);
+            if(count.get(y)==0){
+                nonzero--;
+            }else if(count.get(y)==-1){
+                nonzero++;
+            }
+            if(nonzero==0){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
