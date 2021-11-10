39e211e8760569b21bebd0e743dadab9b1dbf216
Macoredroid <zhiyuanma0520@gmail.com>
2021-11-10 09:18:56 -0800

    update

---
+++ 339/20211110.java
@@ -0,0 +1,47 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+class Solution {
+    int cnt=0;
+    public int depthSum(List<NestedInteger> nestedList) {
+        for(NestedInteger n:nestedList){
+            dfs(1,n);
+        }
+        return cnt;
+    }
+    
+    public void dfs(int depth,NestedInteger n){
+        if(n.isInteger()){
+            cnt+=n.getInteger()*depth;
+            return;
+        }
+        for(NestedInteger next:n.getList()){
+            dfs(depth+1,next);
+        }
+    }
+}
