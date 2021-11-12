3d393b66b8ce214ec854939bc648f14b3b0d2418
Macoredroid <zhiyuanma0520@gmail.com>
2021-11-12 09:37:32 -0800

    update

---
+++ 766/202111112.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=1;i+j<matrix.length&&j<matrix[0].length;j++){
+                if(matrix[i+j][j]!=matrix[i+j-1][j-1]){
+                    return false;
+                }
+            }
+        }
+        
+        for(int i=1;i<matrix[0].length;i++){
+            for(int j=1;i+j<matrix[0].length&&j<matrix.length;j++){
+                if(matrix[j][i+j]!=matrix[j-1][i+j-1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
